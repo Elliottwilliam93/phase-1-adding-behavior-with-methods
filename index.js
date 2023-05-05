d57c16e055c2d40d960bf5d17208bc22c9f355dc
williamelliott <ironmaiden229@hotmail.com>
2023-05-05 15:12:32 -0400

    project done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Cat{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+let cat = new Cat
+
+class Dog{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+let dog = new Dog
+
+class Bird{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`
+    } else {
+        return `${this.name} says squawk!`
+      }
+    }
+}
+
+let bird = new Bird
+let bird2 = new Bird
